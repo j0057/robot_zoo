d700bd5ba876b577fb10c6437f9be8132ed8bb27
Joost Molenaar <j.j.molenaar@gmail.com>
2014-09-05 20:42:25 +0200

    Finally! A real package is born!

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env/python2.7
+
+from setuptools import setup
+
+setup(name='twitter',
+      author='Joost Molenaar',
+      author_email='j.j.molenaar@gmail.com',
+      version_command='git describe',
+      packages=['robot_zoo'],
+      data_files=[ ('bin', 'bin/mail-log') ])
