c437b6f1998f51c97036b2ae05e20afbed78e09f
Joost Molenaar <j.j.molenaar@gmail.com>
2014-09-06 22:04:40 +0200

    Add requirements to setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,17 @@
 
 from setuptools import setup
 
+try:
+    with open('requirements.txt', 'r') as f:
+        requirements = f.read().split()
+except IOError:
+    with open('twitter.egg-info/requires.txt', 'r') as f:
+        requirements = f.read().split()
+
 setup(name='twitter',
       author='Joost Molenaar',
       author_email='j.j.molenaar@gmail.com',
       version_command='git describe',
       packages=['robot_zoo'],
-      data_files=[ ('bin', ['bin/mail-log']) ])
+      data_files=[ ('bin', ['bin/mail-log']) ],
+      install_requires=requirements)
