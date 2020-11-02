35f6b30b22fa09b9f83fa1047c409ef04a614987
Joost Molenaar <jjm@j0057.nl>
2020-11-02 15:25:02 +0100

    Detect Koningsdag in years where it would ordinarily fall on a Sunday

--- robot_zoo/holidays.py
+++ robot_zoo/holidays.py
@@ -30,14 +30,17 @@ class Holidays(object):
     def easter(self, days):
         return self.easter_date + dateutil.relativedelta.relativedelta(days=days)
 
+    def is_sunday(self, month, day):
+        return datetime.date(self.year, month, day).weekday() == 6
+
 class NL(Holidays):
     def __init__(self, year):
         super(NL, self).__init__(year,
             ("Nieuwjaarsdag",                   lambda date: date == self.day(1, 1)),
             ("Eerste Paasdag",                  lambda date: date == self.easter(+0)),
             ("Tweede Paasdag",                  lambda date: date == self.easter(+1)),
-            ("Koninginnedag",                   lambda date: date == self.day(4, 30) and (1949 <= date.year <= 2013)),
-            ("Koningsdag",                      lambda date: date == self.day(4, 27) and (2014 <= date.year)),
+            ("Koninginnedag",                   lambda date: date == self.day(4, 30 - self.is_sunday(4, 30)) and (1949 <= date.year <= 2013)),
+            ("Koningsdag",                      lambda date: date == self.day(4, 27 - self.is_sunday(4, 27)) and (2014 <= date.year)),
             ("Dodenherdenking",                 lambda date: date == self.day(5, 4)),
             ("Bevrijdingsdag",                  lambda date: date == self.day(5, 5)),
             ("Hemelvaartsdag",                  lambda date: date == self.easter(+39)),
